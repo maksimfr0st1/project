00c24fe730d22cca31d67b16cfa7d785200e8650
Maksim <example@gmail.com>
2023-02-09 12:01:03 +0300

    first commit

---
+++ Р2 У6/js/scirp.js
@@ -0,0 +1,30 @@
+'use strict'
+// 2.12 
+// // alert('Hello world!')
+
+// // const result = confirm('Are you here?');
+// // console.log(result);
+
+// // const answer = prompt('Вам есть 18?', '18');
+// // console.log(typeof(answer))
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Как ваше фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// ////////////////////////////////////////////////////
+
+// 2.13
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Maxim';
+
+alert(`Привет, ${user}`);
